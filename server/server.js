ad4177c932adcf42a5faa1a24fde285c6bde4f03
abhinav anshul <abhinavanshul4u@gmail.com>
2019-02-17 09:02:11 +0530

    Greet new users and  alert users

--- server/server.js
+++ server/server.js
@@ -14,7 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  
+  // socket.emit from admin text welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.broadcast.emit from admin text new user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -22,6 +35,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date.getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
